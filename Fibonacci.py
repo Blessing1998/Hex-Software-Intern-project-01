a358e4fcd17847b769cdae74c0655dc14d4aa669
Blessing <81161483+Blessing1998@users.noreply.github.com>
2025-02-05 08:22:38 +0200

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,20 @@
+#HEX SOFTWARE INTERNSHIP
+# python code to generate infinite fibonacci series using generator
+# In the following program
+# we write a function fibonacci() 
+# that computes nth element of a Fibonacci series using recursion as follows,
+
+def fibonacci(n):
+	if n<=1:
+		return n
+	else:
+		return(fibonacci(n-1) + fibonacci(n-2))
+
+n = int(input('Enter a number, N, N>=2 : '))
+
+fibo_series = []
+
+for i in range(0,n):
+	fibo_series.append(fibonacci(i))
+	
+print(fibo_series)
